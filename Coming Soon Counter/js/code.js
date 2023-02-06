456a3131f512807e2b3d775f47509be0226e8034
Svendolin <sven0815@gmx.ch>
2023-02-06 19:52:28 +0100

    Finished the Countdown Replacement to launch it on the Website

---
+++ Coming Soon Counter/js/code.js
@@ -0,0 +1,31 @@
+// function to set the date we're counting down to a specific date:
+
+let countDownDate = new Date("Mar 1, 2023 00:00:00").getTime();
+// Update the count down every 1 second:
+let x = setInterval(function () {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+    // Store the result in variables:
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    // Display the result in the element with innerHTML:
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // If the count down is finished, write some text like 00 everywhere:
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+    }
+
+}, 1000);
